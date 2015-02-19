d27d6af6ac1eba6e2af5c47c66580b6b1c06808e
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-02-19 03:33:48 +0000

    added task_01.py and task_02.py and task_03.py

---
+++ task_01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is a really good doctring"""
+
+MY_ANSWER = raw_input('What is your name? ')
+
+print '{}'.format(MY_ANSWER)
---
+++ task_02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Using raw_input as an integer."""
+
+MY_INTEGER = int(raw_input('How old are you? '))
+print '{}'.format(MY_INTEGER)
