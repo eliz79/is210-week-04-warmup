f8487889116c0570a4104f267a64979a8f6323b4
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-02-21 01:00:10 +0000

    trying to correct task_05.py

--- task_05.py
+++ task_05.py
@@ -3,13 +3,23 @@
 """This is a branching statement"""
 
 BP_STATUS = raw_input('What is your blood pressure? ')
+BP_STATUS = int(BP_STATUS)
 
-if BP_STATUS > 89:
+if BP_STATUS <= 89:
     print 'LOW!'
 
-if BP_STATUS >= 90 and <= 119:
+else:
+    BP_STATUS <= 90 or =>119
     print 'IDEAL'
 
-       
+#if BP_STATUS <=120 or 159:
+    print 'WARNING!'
+
+#if BP_STATUS =<140 or >=159
+    print 'HIGH!'
+
+#if BP_STATUS =<160
+    print 'EMERGENCY!'
+
 print '{}'.format(BP_STATUS)
 
