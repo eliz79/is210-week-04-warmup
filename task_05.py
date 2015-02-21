5b76c35232d3b609381e49fc3323c1e802f08172
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-02-21 15:01:32 +0000

    changed task 5

--- task_05.py
+++ task_05.py
@@ -4,22 +4,29 @@
 
 BP_STATUS = raw_input('What is your blood pressure? ')
 BP_STATUS = int(BP_STATUS)
+#BLOOD_PRESSURE = raw_input('Your status is currently: ')
+#LOW = 'LOW!'
+#IDEAL = 'IDEAL!'
+#WARNING = 'WARNING!'
+#HIGH = 'HIGH'
+#EMERGENCY = 'EMERGENCY'
 
 if BP_STATUS <= 89:
-    print 'LOW!'
+    print 'Your status is currently: LOW!'
+
+if 90 < BP_STATUS > 119:
+    print 'Your status is currently: IDEAL'
+
+if 120 < BP_STATUS < 139:
+    print 'Your status is currently: WARNING!'
+
+if 140 < BP_STATUS > 159:
+    print 'Your status is currently: HIGH!'
 
 else:
-    BP_STATUS <= 90 or =>119
-    print 'IDEAL'
-
-#if BP_STATUS <=120 or 159:
-    print 'WARNING!'
-
-#if BP_STATUS =<140 or >=159
-    print 'HIGH!'
-
-#if BP_STATUS =<160
-    print 'EMERGENCY!'
+    BP_STATUS < 160
+    print 'Your status is currently: EMERGENCY!'
 
 print '{}'.format(BP_STATUS)
+#print '{}'.format(LOW, IDEAL, WARNING, HIGH, EMERGENCY)
 
